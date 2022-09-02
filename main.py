8726f6afb0bf9291032388a1084a9e5b229839c7
map3101 <marcelo.postiglione@gmail.com>
2022-09-02 19:58:10 -0300

    feat: Custom db cli command

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db)
+
+
+@app.cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
