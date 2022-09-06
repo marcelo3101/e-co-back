bd589abaae22cbae8f3927aed1d781359ee00218
Anon1929 <anon89345@outlook.com>
2022-09-06 14:30:10 -0300

    Entrada de usuario

--- app/blueprint_api/users.py
+++ app/blueprint_api/users.py
@@ -1,7 +1,19 @@
 from flask import jsonify
 
 from . import api
-
-@api.route("/test")
-def test():
-    return jsonify(message="Funcionando")
+from app import db
+from app.models import Usuario
+@api.route("/user", methods=["POST"])
+def get_)user():
+    request_data = request.get_json()
+    user_id = request_data.get("user_id")
+    if not(user_id):
+        return jsonify(erro="Insira o CPF"), 400
+    else:
+        user = Usuario.query.filter_by(id=user_id)
+        return jsonify(
+            id= user.id,
+            email = user.email,
+            nome = user.nome,
+            ecopoints = user.ecopoints
+        )
