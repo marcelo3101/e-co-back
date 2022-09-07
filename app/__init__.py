20303d29debfdbc9794e1854d0d854483e7741ea
map3101 <marcelo.postiglione@gmail.com>
2022-09-07 15:00:06 -0300

    feat: Configure cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,11 +11,13 @@ class Config:
     FLASK_DEBUG=1
 
 db = SQLAlchemy()
+cors = CORS()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    cors.init_app(app)
     
     from .blueprint_api import api
     app.register_blueprint(api, url_prefix="/api/")
