7837fb50c2e684e4582d735c3619751fdf847eea
map3101 <marcelo.postiglione@gmail.com>
2022-09-06 21:38:34 -0300

    refactor: Create and confirm delivery logic

--- app/blueprint_api/deliveries.py
+++ app/blueprint_api/deliveries.py
@@ -17,19 +17,16 @@ def create_delivery():
                 - categoria
     """
     request_data = request.get_json()
-    user_id = request_data.get("user_id")
     ponto_coleta_id = request_data.get("ponto_coleta_id")
     nome_produto = request_data.get("nome_produto")
     categoria = request_data.get("categoria")
     if not (
-        user_id and
         ponto_coleta_id and
         nome_produto and
         categoria
     ):
         return jsonify(erro="Parâmetros faltando"), 400
     entrega = Entrega(
-        usuario=user_id,
         ponto_coleta=ponto_coleta_id,
         nome_produto=nome_produto,
         categoria=categoria,
@@ -50,23 +47,26 @@ def create_delivery():
 api.route("/delivery/confirm/<int:id>")  # Rota para simular a confirmação de uma entrega
 def confirm_delivery(id):
     """
-        Simular como se a entrega tivesse sido aprovada
+        Rota que simula o scan do qrcode para confirmação da entrega
+        Garante +600 ecopoints
         -----
         parameters:
             - JSON:
-                - ecopoints
+                - user_id
     """
     entrega: Entrega = Entrega.query.get_or_404(id)
     if entrega.estado != 1:
         return jsonify(erro="Estado da entrega inválido"), 400
 
     request_data = request.get_json()
-    ecopoints = request_data.get("ecopoints")
-    if not ecopoints:
-        return jsonify(erro="Quantidade de ecopoints faltando"), 400
-    entrega.pontuacao = ecopoints
-    usuario: Usuario = Usuario.query.get(entrega.usuario)
-    usuario.ecopoints += ecopoints
+    user_id = request_data.get("user_id")
+    if not user_id:
+        return jsonify(erro="Id do usuário faltando"), 400
+    
+    entrega.pontuacao = 600
+    entrega.usuario = user_id
+    usuario: Usuario = Usuario.query.get(user_id)
+    usuario.ecopoints += 600
     db.session.bulk_save_objects([entrega, usuario])
     return jsonify(message="Entrega confirmada"), 200
 
